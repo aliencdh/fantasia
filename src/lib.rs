d09ffeb48a0c0c7a31733886c6d696524c6b5674
aliencdh <cdevhax@gmail.com>
2023-04-18 21:00:42 +0200

    line drawing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Mul, Sub};
+
 #[derive(Clone)]
 pub struct Renderer {
     buffer: Vec<u8>,
@@ -18,15 +20,36 @@ impl Renderer {
     }
 
     pub fn clear(&mut self, color: Rgba) {
-        // Since we're using RGBA, we can expect every pixel to take up 4 bytes.
         self.buffer = std::iter::repeat(color)
             .flat_map(|color| color.bytes())
             .take(self.buffer.len())
             .collect();
     }
+
+    pub fn line(&mut self, color: Rgba, from: Point, to: Point) {
+        for t in 0..100 {
+            let t = t as f32 / 100.;
+            let current = t * (to - from);
+
+            self.put_pixel(self.to_screen_coords(from + current), color);
+        }
+    }
+
+    pub fn to_screen_coords(&self, point: Point) -> (usize, usize) {
+        (
+            ((point.x + 1.) * self.width as f32 / 2.) as usize,
+            ((point.y + 1.) * self.height as f32 / 2.) as usize,
+        )
+    }
+
+    pub fn put_pixel(&mut self, coords: (usize, usize), color: Rgba) {
+        let idx = coords.0 + coords.1 * self.width;
+        self.buffer
+            .splice(idx..idx + 4, color.bytes().iter().cloned());
+    }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct Rgba {
     r: u8,
     g: u8,
@@ -40,5 +63,55 @@ impl Rgba {
 
     pub fn bytes(&self) -> [u8; 4] {
         [self.r, self.g, self.b, self.a]
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Debug, Default)]
+pub struct Point {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+impl Point {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn magnitude(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
+    }
+}
+impl Add for Point {
+    type Output = Self;
+    fn add(mut self, rhs: Self) -> Self::Output {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+        self
+    }
+}
+impl Sub for Point {
+    type Output = Self;
+    fn sub(mut self, rhs: Self) -> Self::Output {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self.z -= rhs.z;
+        self
+    }
+}
+/// Scalar Multiplication
+impl Mul<f32> for Point {
+    type Output = Self;
+    fn mul(mut self, rhs: f32) -> Self::Output {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+        self
+    }
+}
+impl Mul<Point> for f32 {
+    type Output = Point;
+    fn mul(self, rhs: Point) -> Self::Output {
+        rhs * self
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,14 @@ fn main() {
     let mut renderer = Renderer::new(WIDTH, HEIGHT);
     renderer.clear(Rgba::new(0, 0, 0, 255));
 
+    let a = Point::new(0., 1., 0.);
+    let b = Point::new(-1., 0., 0.);
+    let c = Point::new(1., 0., 0.);
+
+    renderer.line(Rgba::new(255, 255, 255, 255), a, b);
+    renderer.line(Rgba::new(255, 255, 255, 255), b, c);
+    renderer.line(Rgba::new(255, 255, 255, 255), c, a);
+
     let buf = image::RgbaImage::from_raw(WIDTH as u32, HEIGHT as u32, renderer.buffer().to_vec())
         .unwrap();
 
